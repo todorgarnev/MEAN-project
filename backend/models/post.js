fbf2e3a237ee64ca3a2815ea62a9cb8450c8d9fd
Todor Garnev <tgarnev@abv.bg>
2018-11-03 14:02:04 +0200

    Added mongoose

---
+++ backend/models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  content: {
+    type: String,
+    require: true
+  }
+});
