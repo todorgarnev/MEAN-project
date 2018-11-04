cda1680755f208c35e4e8100530c16ecad129f1a
Todor Garnev <tgarnev@abv.bg>
2018-11-04 15:20:45 +0200

    Fixed bug on saving uploaded images name.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -14,14 +14,17 @@ const MIME_TYPE_MAP = {
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error('Invalid mimetype');
+    let error = new Error('Invalid mime type');
     if (isValid) {
       error = null;
     }
     cb(error, 'backend/images');
   },
-  fileName: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
+  filename: (req, file, cb) => {
+    const name = file.originalname
+      .toLowerCase()
+      .split(' ')
+      .join('-');
     const ext = MIME_TYPE_MAP[file.mimetype];
     cb(null, name + '-' + Date.now() + '.' + ext);
   }
