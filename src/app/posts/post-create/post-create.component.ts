8760a94c4862051933575f7346d8aeb9c625fdec
Todor Garnev <tgarnev@abv.bg>
2018-11-04 13:53:21 +0200

    Added custom async validatior to image upload.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -16,17 +17,18 @@ export class PostCreateComponent implements OnInit {
   public post: Post;
   public isLoading = false;
   public form: FormGroup;
+  public imagePreview: string;
   private mode = 'create';
   private postId: string;
 
   constructor(public postsService: PostsService,
-    public route: ActivatedRoute,
-    private fb: FormBuilder) { }
+    public route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.form = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(3)]],
-      content: ['', Validators.required]
+    this.form = new FormGroup({
+      title: new FormControl(null, { validators: [Validators.required, Validators.minLength(3)] }),
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, { validators: [Validators.required], asyncValidators: [mimeType] })
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -47,6 +49,17 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
